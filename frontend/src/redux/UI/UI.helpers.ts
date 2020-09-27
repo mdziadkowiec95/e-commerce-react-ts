bf87fa0d51412f83cec442be5662dbcf3188a4b7
Michal Dziadkowiec <michal.dziadkowiec@motorolasolutions.com>
2020-09-27 15:48:08 +0200

    Fix formatting

--- frontend/src/redux/UI/UI.helpers.ts
+++ frontend/src/redux/UI/UI.helpers.ts
@@ -2,55 +2,55 @@ import { Category, NavCategories, NavCategory } from "../../common/types/categor
 
 // Map single contentful category entry to extract only needed data
 const mapCategory = (category: Category): NavCategory => {
-	const { categoryTree, displayName } = category.fields;
-	const { id } = category.sys;
+  const { categoryTree, displayName } = category.fields;
+  const { id } = category.sys;
 
-	return {
-		id,
-		categoryTree,
-		displayName,
-	};
+  return {
+    id,
+    categoryTree,
+    displayName,
+  };
 };
 
 // Map contentful categories data structure to make it easier to render deep navigation
 export const mapCategories = (categories: Category[]): NavCategories => {
-	return categories.reduce(
-		(result: NavCategories, category: Category): NavCategories => {
-			const { parentCategory, categoryTree } = category.fields;
+  return categories.reduce(
+    (result: NavCategories, category: Category): NavCategories => {
+      const { parentCategory, categoryTree } = category.fields;
 
-			if (!categoryTree) return result;
+      if (!categoryTree) return result;
 
-			if (!parentCategory) {
-				if (result[categoryTree]) {
-					return result;
-				} else {
-					return {
-						...result,
-						[categoryTree]: {
-							...mapCategory(category),
-							subcategories: [],
-						},
-					};
-				}
-			} else {
-				const parentCategoryFieldName = parentCategory.fields.categoryTree;
+      if (!parentCategory) {
+        if (result[categoryTree]) {
+          return result;
+        } else {
+          return {
+            ...result,
+            [categoryTree]: {
+              ...mapCategory(category),
+              subcategories: [],
+            },
+          };
+        }
+      } else {
+        const parentCategoryFieldName = parentCategory.fields.categoryTree;
 
-				if (result[parentCategoryFieldName]) {
-					result[parentCategoryFieldName].subcategories?.push(
-						mapCategory(category)
-					);
-					return result;
-				} else {
-					return {
-						...result,
-						[`${parentCategoryFieldName}`]: {
-							...mapCategory(parentCategory),
-							subcategories: [mapCategory(category)],
-						},
-					};
-				}
-			}
-		},
-		{}
-	);
+        if (result[parentCategoryFieldName]) {
+          result[parentCategoryFieldName].subcategories?.push(
+            mapCategory(category)
+          );
+          return result;
+        } else {
+          return {
+            ...result,
+            [`${parentCategoryFieldName}`]: {
+              ...mapCategory(parentCategory),
+              subcategories: [mapCategory(category)],
+            },
+          };
+        }
+      }
+    },
+    {}
+  );
 }

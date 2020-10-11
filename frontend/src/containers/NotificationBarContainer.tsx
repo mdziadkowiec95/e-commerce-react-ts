5ab43fce146f50c98c3c11a63c5fb1549f64a03f
Michal Dziadkowiec <michal.dziadkowiec@motorolasolutions.com>
2020-10-11 14:32:00 +0200

    Refactor connect() to redux hooks

--- frontend/src/containers/NotificationBarContainer.tsx
+++ frontend/src/containers/NotificationBarContainer.tsx
@@ -1,40 +1,24 @@
 import React from 'react';
-import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import NotificationBar, { Notification } from '../components/NotificationBar';
 import { NotificationsActions } from '../redux/Notifications/notifications.reducer';
-import * as NotificationThunks from '../redux/Notifications/notifications.thunks';
 import { RootState } from '../redux/rootReducer';
+import { useAppDispatch } from '../redux/store';
 
-interface Props {
-  notifications: Notification[];
-  addNotification: (
-    notification: Notification,
-    seconds?: number
-  ) => Promise<void>;
-  removeNotification: ActionCreatorWithPayload<{ id: string }>;
-}
+const NotificationBarContainer = () => {
+  const dispatch = useAppDispatch();
+  const notifications: Notification[] = useSelector(
+    (state: RootState) => state.notifications
+  );
 
-const NotificationBarContainer = ({
-  notifications,
-  removeNotification,
-}: Props) => (
-  <NotificationBar
-    notifications={notifications}
-    onNotificationDelete={(id: string) => removeNotification({ id })}
-  />
-);
-
-const mapStateToProps = ({ notifications }: RootState) => ({
-  notifications,
-});
-
-const mapDispatchToProps = {
-  addNotification: NotificationThunks.addNotification,
-  removeNotification: NotificationsActions.removeNotification,
+  return (
+    <NotificationBar
+      notifications={notifications}
+      onNotificationDelete={(id: string) =>
+        dispatch(NotificationsActions.removeNotification({ id }))
+      }
+    />
+  );
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(NotificationBarContainer);
+export default NotificationBarContainer;

0e3fc3bbdde79d6bf4a8c9db17ac08694227874e
Michal Dziadkowiec <michal.dziadkowiec@motorolasolutions.com>
2020-10-24 15:53:00 +0200

    List basic info in minicart

--- frontend/src/components/MiniCart/MiniCart.test.tsx
+++ frontend/src/components/MiniCart/MiniCart.test.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MiniCart from '.';
+import { CartState } from '../../redux/Cart/cart.reducer';
+
+const cartMock: CartState = {
+  products: [],
+  isLoading: false,
+};
+
+const userMock = {
+  isAuth: false,
+  isLoading: true,
+  user: null,
+};
 
 describe('<MiniCart />', () => {
   test('should render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<MiniCart />, div);
+    ReactDOM.render(<MiniCart cart={cartMock} user={userMock} />, div);
   });
 });
--- frontend/src/components/MiniCart/index.tsx
+++ frontend/src/components/MiniCart/index.tsx
@@ -1,7 +1,13 @@
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React, { MouseEvent } from 'react';
+import { CURRENCY } from '../../common/baseConfig/price';
+import { getTotalPrice } from '../../common/prices/getTotalPrice';
+import { ProductInCart } from '../../common/types/product';
+import { User } from '../../common/types/user';
+import { CartState } from '../../redux/Cart/cart.reducer';
 import ButtonIcon from '../ButtonIcon';
 import Popover from '../Popover';
+import styles from './MiniCart.module.scss';
 
 interface ButtonProps {
   onClick: (e: MouseEvent<HTMLButtonElement>) => void;
@@ -16,14 +22,42 @@ const MiniCartButton = ({ onClick }: ButtonProps) => (
   ></ButtonIcon>
 );
 
-const MiniCart = () => {
+interface Props {
+  cart: CartState;
+  user: {
+    isLoading: boolean;
+    isAuth: boolean;
+    user: User | null;
+  };
+}
+
+const MiniCart = ({ cart, user }: Props) => {
   return (
-    <Popover alignRight buttonComponent={MiniCartButton}>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+    <Popover alignRight buttonComponent={MiniCartButton} mouseDelay={0}>
+      <div className={styles.contentWrap}>
+        {cart &&
+          cart.products &&
+          cart.products.length > 0 &&
+          cart.products.map((product: ProductInCart) => (
+            <div className={`media ${styles.itemWrap}`}>
+              <div className="media-left">
+                <figure
+                  className={`image is-48x48 ${styles.image}`}
+                  style={{ backgroundImage: `url(${product.image.url})` }}
+                ></figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-6">{product.productName}</p>
+                <span>
+                  {product.price * product.quantity} {CURRENCY}
+                </span>
+              </div>
+            </div>
+          ))}
+        <p>
+          Summary: {getTotalPrice(cart.products)} {CURRENCY}
+        </p>
+      </div>
     </Popover>
   );
 };
